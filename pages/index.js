bcae37b296c3c55104d14ff6a1969e781320b343
Shmulik Flint <shmulikf@wix.com>
2020-10-11 01:09:08 +0300

    another gtm attempt

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,16 @@ export default function Home({ dates }) {
         <meta property="og:image" content={`https://yeruham-corona-status.now.sh/logo.png`} key="image"/>
         <link rel="icon" href="/favicon.ico" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180152901-1"/>
-        <script>window.dataLayer = (window.dataLayer || []).concat(window.dataLayer, 'js', new Date(), 'config', 'UA-180152901-1')</script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-180152901-1');
+          `,
+          }}
+        />
       </Head>
 
       <main>
